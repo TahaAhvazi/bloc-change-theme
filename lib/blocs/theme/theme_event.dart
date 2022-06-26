c3a09f4a6fc91f7fd6b188485468550e26d9f195
Taha <tahax.dev@gmail.com>
2022-06-26 16:43:22 +0430

    Save theme event

--- lib/blocs/theme/theme_event.dart
+++ lib/blocs/theme/theme_event.dart
@@ -6,3 +6,18 @@ abstract class ThemeEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class ChangeThemeEvent extends ThemeEvent {
+  final int randomInt;
+  const ChangeThemeEvent(this.randomInt);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'ChangeTheme(Random int is : $randomInt)';
+  }
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [randomInt];
+}
